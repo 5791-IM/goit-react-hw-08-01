39f6e9166c99820f689ba39df573393fc257afa2
5791-IM <142788119+5791-IM@users.noreply.github.com>
2024-06-30 21:37:41 +0300

    ++---

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import ContactList from "../../components/ContactList/ContactList";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
-import ContactList from "../../components/ContactList/ContactList";
 import Loader from "../../components/Loader/Loader";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "../../redux/contacts/operations";
 import DocumentTitle from "../../components/DocumentTitle/DocumentTitle";
 import {
@@ -14,10 +14,11 @@ import {
 import css from "./ContactsPage.module.css";
 
 export default function ContactsPage() {
+  const contacts = useSelector(selectContacts) || [];
+  const loader = useSelector(selectLoading) || false;
+  const error = useSelector(selectError) || null;
+
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const loader = useSelector(selectLoading);
-  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -25,11 +26,12 @@ export default function ContactsPage() {
 
   return (
     <div className={css.container}>
-      <DocumentTitle>Your Contacts</DocumentTitle>
+      {/* <h2>My ContactBook</h2> */}
+      <DocumentTitle>My ContactsBook</DocumentTitle>
       <ContactForm />
       <SearchBox />
       {loader && <Loader />}
-      {contacts.length > 0 && <ContactList />}
+      {contacts.length > 0 && <ContactList contacts={contacts} />}
       {error && <b>Unknown error. Try to reload the page.</b>}
     </div>
   );
--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -2,8 +2,12 @@ import { createSelector } from "@reduxjs/toolkit";
 import { selectStatusFilter } from "../filters/selectors";
 
 export const selectLoading = (state) => state.contacts.loading;
+
 export const selectError = (state) => state.contacts.error;
-export const selectContacts = (state) => state.contacts;
+
+export const selectContacts = (state) => {
+  state.contacts.items;
+};
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectStatusFilter],
